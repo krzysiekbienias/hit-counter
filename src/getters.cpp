988495d27c2849ad0fd55cb091debb9da679bb89
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-03-07 16:36:24 +0100

    feat: Implement helper methods (getters) to be able to test methods in more isolated framework.

---
+++ src/getters.cpp
@@ -0,0 +1,14 @@
+#include <interface.hpp>
+
+int HitCounter::getTotalHits() const {
+    return m_totalHits;
+}
+
+
+std::pair<int, int> HitCounter::getLastBucket() const {
+    return m_playlist.back();
+}
+
+int HitCounter::getBucketCount() const {
+    return m_playlist.size();
+}
