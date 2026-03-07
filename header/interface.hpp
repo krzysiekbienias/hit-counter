2e72a1975fc7f3fb84e38a3cc1f634b1cf8d0ec7
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-03-07 16:35:07 +0100

    feat: Declare HitCounter public API.

---
+++ header/interface.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <deque>
+#include <utility>
+
+
+class HitCounter {
+public:
+    HitCounter(){};
+
+    //API
+    void hit(int timestamp);
+    int getHits(int timestamp);
+    //helpers method for unit testes they are public but should not be used
+
+    int getTotalHits() const;
+    int getBucketCount() const;
+    std::pair<int,int> getLastBucket() const;
+
+private:
+
+    std::deque<std::pair<int,int>> m_playlist;
+    int m_totalHits{0};
+
+};
